45da58a119423fe63cdff02c62d3bb2cb65301e2
Tim Mattison <tim@mattison.org>
2020-10-21 11:07:49 -0400

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    kotlin("jvm") version "1.4.10"
+
+    // Creates fat JAR
+    id("com.github.johnrengelman.shadow") version "6.0.0"
+
+    id("java")
+}
+
+java.toolchain.languageVersion.set(JavaLanguageVersion.of(8))
+
+val gradleDependencyVersion = "6.7"
+
+tasks.wrapper {
+    gradleVersion = gradleDependencyVersion
+    distributionType = Wrapper.DistributionType.ALL
+}
+
+repositories {
+    mavenCentral()
+}
+
+tasks.register("temp", {
+    println("Test")
+})
+
+tasks.shadowJar {
+    // Just to show that dependsOn shows as an error as well
+    dependsOn += tasks.get("temp")
+}
